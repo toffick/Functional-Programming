dbb8871ad6ed0330f25b0f498901a31690d76edc
Nick Borsuk <gor_nick987@Mail.ru>
2020-01-19 16:06:45 +0300

    1 task

---
+++ l1/2.js
@@ -0,0 +1,10 @@
+﻿const coinsTypes = new Array(2).fill(0).map((_, i) => i + 1);
+
+function getAllChanses(amount, typeIndex) {
+    if (amount < 0 || typeIndex < 0) return 0;
+    if (amount === 0 || typeIndex === 0) return 1;
+    return getAllChanses(amount, typeIndex - 1) +
+        getAllChanses(amount - coinsTypes[typeIndex], typeIndex);
+}
+
+console.log(getAllChanses(8, coinsTypes.length - 1));

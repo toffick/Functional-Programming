999c824e2b17afd7430482d1ff36d11a39e1698b
Nick Borsuk <gor_nick987@Mail.ru>
2020-01-19 19:30:57 +0300

    task 5
    upd 1 task
    
    fix
    
    fix

--- l1/1.js
+++ l1/1.js
@@ -1,1 +1,22 @@
-﻿
+﻿let result = '';
+
+function foo(rows, currentRow = 0) {
+    if (rows === currentRow) return result;
+
+    result = result.concat('  '.repeat((rows - currentRow)));
+
+    for (let i = 0; i <= currentRow; i++)
+        result = result.concat(`${'    '}${factorial(currentRow) / (factorial(i) * factorial(currentRow - i))}`);
+
+    result = result.concat('\n');
+    foo(rows, ++currentRow);
+}
+
+function factorial(x) {
+    if (x === 0)
+        return 1;
+    return x * factorial(x - 1);
+}
+
+console.log(foo(10))
+console.log(result)
---
+++ l5/index.js
@@ -0,0 +1,35 @@
+﻿const pas = [2, 3, 1, 55];
+const tax = [97, 19, 18, 56];
+
+
+console.log(
+    ((arr, ...arrs) =>
+        arr.map(
+            (val, i) =>
+            arrs.reduce(
+                (a, arr) => [...a, arr[i]], [val]
+            )
+        ))(pas.sort(), tax.sort().reverse())
+);
+
+
+
+const searchString = `public class Person {
+    private final String name;
+    private final int age; final}
+s`;
+
+
+console.log(
+    searchString
+    .toLowerCase()
+    .match(
+        /[a-z]+/g
+    )
+    .reduce(
+        (acc, item) => {
+            acc[item] = acc[item] ? acc[item] + 1 : 1;
+            return acc;
+        }, {}
+    )
+);
